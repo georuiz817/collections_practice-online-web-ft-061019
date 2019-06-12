ac10f884fcdc516ece59425d3b2dafcc9066af8c
George Ruiz <georuiz817@gmail.com>
2019-06-12 16:35:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,5 +19,12 @@ def reverse_array(array)
   array.reverse
 end
 
-def kesha_maker(array)
-  
+ def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
